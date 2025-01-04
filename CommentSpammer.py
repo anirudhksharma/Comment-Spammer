87e604b9da9519a593c6a51de90e923199c4160f
Anirudh Sharma <115159991+anirudhksharma@users.noreply.github.com>
2025-01-03 19:59:50 -0500

    Update CommentSpammer.py

--- CommentSpammer.py
+++ CommentSpammer.py
@@ -1,47 +1,48 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 
 driver = webdriver.Chrome()
-driver.get("https://www.youtube.com/watch?v=YZtzNN6YDwk&ab_channel=vauvau")
+driver.get("Enter video link here")
 time.sleep(5)
 
 
 ##Signing in
 driver.find_element(by=By.LINK_TEXT, value="Sign in").click()
 time.sleep(2)
-driver.find_element(by=By.ID, value="identifierId").send_keys('commentspammer101@gmail.com')
+driver.find_element(by=By.ID, value="identifierId").send_keys('Enter Gmail username here')
 time.sleep(0)
 next = driver.find_element(by=By.ID, value="identifierNext")
 next.click()
 time.sleep(2)
-driver.find_element(by=By.NAME, value="Passwd").send_keys('noobcomments')
+driver.find_element(by=By.NAME, value="Passwd").send_keys('Enter Gmail password here')
 time.sleep(0)
 next = driver.find_element(by=By.ID, value="passwordNext")
 next.click()
-time.sleep(15)
-
-##Adding comment
-text_box = driver.find_element(By.ID, "contenteditable-root")
-text_box.click()
-keyword = "Best video ever"
-text_box.send_keys(keyword)
-time.sleep(5)
-
-comment = driver.find_element(by=By.XPATH, value="submit-button")
-comment.click()
-
-"""
-
 time.sleep(10)
 
-email = "commentspammer101@gmail.com"
-password = "noobcomments"
-video_url = "https://www.youtube.com/watch?v=YZtzNN6YDwk&ab_channel=vauvau" 
-comment_text = "Best video ever"  
+driver.execute_script("window.scrollTo(0, 2500)")
+time.sleep(4)
+driver.execute_script("window.scrollTo(0, 5)")
+driver.execute_script("window.scrollTo(0, 2600)")
+time.sleep(4)
 
-"""
+
+##Adding comment
+i = 0
+comment_count = 1
+while i < comment_count:
+
+
+    text_box = driver.find_element(By.ID, "placeholder-area")
+    text_box.click()
+    time.sleep(0.5)
+    comment_box = driver.find_element(By.ID, "contenteditable-root")
+    keyword = "Enter some comment here"
+    comment_box.send_keys(keyword)
+    time.sleep(0.5)
+
+    comment = driver.find_element(by=By.ID, value="submit-button")
+    comment.click()
+    time.sleep(2)
 
